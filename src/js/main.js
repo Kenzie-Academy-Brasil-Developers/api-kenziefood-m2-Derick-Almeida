71a638d8357a1ae1724fe89837c1f26b84746277
Gabriel Pereira <ggabriel.p2003@gmail.com>
2022-05-17 14:10:54 -0300

    inserindo cards/Loja Pub

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,10 @@
-import PesquisarProduto from "../models/pesquisarPorNome.js";
+import { Api } from "../controller/Api.js";
+import {PesquisarProduto} from "../models/pesquisarPorNome.js";
+import {criarCard} from "./card.js"
+
+const box = document.querySelector(".box")
+console.log(box)
+
+produtos.forEach(element => {criarCard(element)});
 
 PesquisarProduto.filtrarProdutos();
