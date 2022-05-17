b506aa7fbd89419a031137ed3499e75dc5390f35
Matheus <matheus_lsilva@hotmail.com>
2022-05-17 10:40:08 -0300

    fix/removendo comentarios do arquivo login.js

--- src/pages/assets/js/login.js
+++ src/pages/assets/js/login.js
@@ -1,49 +1,5 @@
 import { Api } from "../../../controller/Api.js";
 
-// function eventoBotaoLogin() {
-//   const botao = document.getElementById("btnLogin");
-//   botao.addEventListener("click", pegarDadosFormulario);
-// }
-
-// function pegarDadosFormulario(event) {
-//   event.preventDefault();
-
-//   const formulario = document.querySelector("form");
-//   const elementosFormulario = [...formulario.children];
-//   const dadosLogin = {};
-
-//   elementosFormulario.forEach(elem => {
-//     if (elem.name !== "") {
-//       dadosLogin[elem.name] = elem.value;
-//       console.log(dadosLogin);
-//     }
-//   });
-
-//   fazerLogin(dadosLogin);
-// }
-
-// async function fazerLogin(dadosLogin) {
-//   const response = await Api.loginUsuario(dadosLogin);
-//   const modalDiv = document.getElementById("modalDiv");
-//   modalDiv.classList.add("modal__div");
-//   const modalTexto = document.querySelector(".modal__texto");
-//   if (typeof response === "string") {
-//     localStorage.setItem("token", response);
-//     modalTexto.textContent = "Login efetuado com sucesso!";
-//     setTimeout(() => {
-//       window.location = "../../src/pages/dashboard.html";
-//     }, 3000);
-//   } else {
-//     modalDiv.style.background = "red";
-//     modalTexto.innerText = response.error;
-//     setTimeout(() => {
-//       location.reload();
-//     }, 3000);
-//   }
-// }
-
-// eventoBotaoLogin();
-
 async function loginUser(event) {
   event.preventDefault();
   const email = document.getElementById("email");
