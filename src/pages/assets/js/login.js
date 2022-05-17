3d07cb31cae21af0d968f2fba51dc01e0574cea6
Matheus <matheus_lsilva@hotmail.com>
2022-05-17 10:24:13 -0300

    feat/reformulacao das telas login e cadastro

--- src/pages/assets/js/login.js
+++ src/pages/assets/js/login.js
@@ -1,43 +1,60 @@
 import { Api } from "../../../controller/Api.js";
 
-function eventoBotaoLogin() {
-  const botao = document.getElementById("btnLogin");
-  botao.addEventListener("click", pegarDadosFormulario);
-}
+// function eventoBotaoLogin() {
+//   const botao = document.getElementById("btnLogin");
+//   botao.addEventListener("click", pegarDadosFormulario);
+// }
 
-function pegarDadosFormulario(event) {
+// function pegarDadosFormulario(event) {
+//   event.preventDefault();
+
+//   const formulario = document.querySelector("form");
+//   const elementosFormulario = [...formulario.children];
+//   const dadosLogin = {};
+
+//   elementosFormulario.forEach(elem => {
+//     if (elem.name !== "") {
+//       dadosLogin[elem.name] = elem.value;
+//       console.log(dadosLogin);
+//     }
+//   });
+
+//   fazerLogin(dadosLogin);
+// }
+
+// async function fazerLogin(dadosLogin) {
+//   const response = await Api.loginUsuario(dadosLogin);
+//   const modalDiv = document.getElementById("modalDiv");
+//   modalDiv.classList.add("modal__div");
+//   const modalTexto = document.querySelector(".modal__texto");
+//   if (typeof response === "string") {
+//     localStorage.setItem("token", response);
+//     modalTexto.textContent = "Login efetuado com sucesso!";
+//     setTimeout(() => {
+//       window.location = "../../src/pages/dashboard.html";
+//     }, 3000);
+//   } else {
+//     modalDiv.style.background = "red";
+//     modalTexto.innerText = response.error;
+//     setTimeout(() => {
+//       location.reload();
+//     }, 3000);
+//   }
+// }
+
+// eventoBotaoLogin();
+
+async function loginUser(event) {
   event.preventDefault();
+  const email = document.getElementById("email");
+  const password = document.getElementById("password");
 
-  const formulario = document.querySelector("form");
-  const elementosFormulario = [...formulario.children];
-  const dadosLogin = {};
-  elementosFormulario.forEach(elem => {
-    if (elem.name !== "") {
-      dadosLogin[elem.name] = elem.value;
-    }
+  const USER_DATAS = await Api.loginUsuario({
+    email: email.value,
+    password: password.value
   });
 
-  fazerLogin(dadosLogin);
+  return USER_DATAS;
 }
-
-async function fazerLogin(dadosLogin) {
-  const response = await Api.loginUsuario(dadosLogin);
-  const modalDiv = document.getElementById("modalDiv");
-  modalDiv.classList.add("modal__div");
-  const modalTexto = document.querySelector(".modal__texto");
-  if (typeof response === "string") {
-    localStorage.setItem("token", response);
-    modalTexto.textContent = "Login efetuado com sucesso!";
-    setTimeout(() => {
-      window.location = "../../src/pages/dashboard.html";
-    }, 3000);
-  } else {
-    modalDiv.style.background = "red";
-    modalTexto.innerText = response.error;
-    setTimeout(() => {
-      location.reload();
-    }, 3000);
-  }
-}
-
-eventoBotaoLogin();
+const BUTTON_LOGIN = document.getElementById("btnLogin");
+BUTTON_LOGIN.addEventListener("click", loginUser);
