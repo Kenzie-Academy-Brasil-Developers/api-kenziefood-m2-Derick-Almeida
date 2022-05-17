c8136ed74d14d7c7bdf66a03944ecc9fe9173fd0
fjrjdev <thedromemafia@gmail.com>
2022-05-17 19:39:28 -0300

    feat: dom produto do carrinho

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,5 @@
 import { Api } from "../controller/Api.js";
+import { Carrinho } from "../models/carrinho.js";
 import { PesquisarProduto } from "../models/pesquisarPorNome.js";
 import { criarCard } from "./card.js";
 
@@ -8,5 +9,8 @@ let produtos = await Api.produtos();
 produtos.forEach(element => {
   criarCard(element);
 });
+
+
+// Carrinho.criarProduto(produtos[0])
 
 PesquisarProduto.filtrarProdutos();
