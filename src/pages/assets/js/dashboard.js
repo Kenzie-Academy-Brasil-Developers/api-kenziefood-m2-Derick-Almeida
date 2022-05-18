fb0e1d640d9901cdd6c426ff3a2cdcf404e80251
Derick <d.a.s2000silva14@gmail.com>
2022-05-18 12:06:20 -0300

    feat - aAdicionando Formularios

--- src/pages/assets/js/dashboard.js
+++ src/pages/assets/js/dashboard.js
@@ -1,65 +1,10 @@
 import { Api } from "../../../controller/Api.js";
+import { Templates } from "../../../models/DashboardTemplates.js";
 
 const token = localStorage.getItem('token');
 const meusProdutos = await Api.produtosCriados(JSON.parse(token));
 
-async function listarProdutos(array) {
-    const lista = document.querySelector('.corpo__tabela');
-    lista.innerHTML = "";
-
-    array.forEach(produto => {
-        const item = document.createElement('li');
-        const caixa1 = document.createElement('div');
-        const imagemProduto = document.createElement('img');
-        const nomeProduto = document.createElement('p');
-
-        const caixa2 = document.createElement('div');
-        const categoriaProduto = document.createElement('span');
-
-        const caixa3 = document.createElement('div');
-        const descricaoProduto = document.createElement('p');
-
-        const caixa4 = document.createElement('div');
-        const botaoEditar = document.createElement('button');
-        const botaoRemover = document.createElement('button');
-
-        item.classList.add('corpo__tabela--linha');
-        caixa1.classList.add('box', 'produto__nome');
-        nomeProduto.classList.add('corpo__tabela--nome');
-
-        caixa2.classList.add('box');
-        categoriaProduto.classList.add('corpo__tabela--categoria');
-
-        caixa3.classList.add('box');
-        descricaoProduto.classList.add('corpo__tabela--descricao');
-
-        caixa4.classList.add('box');
-        botaoEditar.classList.add('corpo__tabela--botao');
-        botaoRemover.classList.add('corpo__tabela--botao');
-
-        botaoEditar.id = 'btnEditar';
-        botaoRemover.id = 'btnRemover';
-
-        imagemProduto.src = produto.imagem;
-        imagemProduto.alt = produto.nome;
-        nomeProduto.innerText = produto.nome;
-
-        categoriaProduto.innerText = produto.categoria;
-
-        descricaoProduto.innerText = produto.descricao;
-
-        botaoEditar.innerText = 'Editar';
-        botaoRemover.innerText = 'Remover';
-
-        caixa1.append(imagemProduto, nomeProduto);
-        caixa2.appendChild(categoriaProduto);
-        caixa3.appendChild(descricaoProduto);
-        caixa4.append(botaoEditar, botaoRemover);
-        item.append(caixa1, caixa2, caixa3, caixa4);
-        lista.appendChild(item);
-    });
-}
-listarProdutos(meusProdutos);
+Templates.listarMeusProdutos(meusProdutos);
 
 function filtrarProdutos() {
     const input = document.getElementById('buscarProduto');
@@ -78,6 +23,57 @@ function buscar(event) {
         return nomeProdutoFormatado.includes(campoDeBuscaFormatado)
     });
 
-    listarProdutos(filtro);
+    Templates.listarMeusProdutos(filtro);
+    editarProduto();
+    excluirProduto();
 }
-filtrarProdutos()
+filtrarProdutos();
+
+function adicionarProduto() {
+    const btnAdicionarProduto = document.getElementById('btnAdicionarProduto');
+    btnAdicionarProduto.addEventListener('click', abrirFormularioAdicionar);
+}
+
+function abrirFormularioAdicionar() {
+    Templates.adicionarProduto();
+
+    const botaoFechar = document.getElementById('btnFechar');
+    botaoFechar.addEventListener('click', () => {
+        const container = document.querySelector('.container');
+        document.body.removeChild(container);
+    });
+}
+adicionarProduto();
+
+function editarProduto() {
+    const btnEditar = document.getElementById('btnEditar');
+    btnEditar.addEventListener('click', abrirFormularioEditar);
+}
+
+function abrirFormularioEditar() {
+    Templates.atualizarProduto();
+
+    const botaoFechar = document.getElementById('btnFechar');
+    botaoFechar.addEventListener('click', () => {
+        const container = document.querySelector('.container');
+        document.body.removeChild(container);
+    });
+}
+editarProduto();
+
+
+function excluirProduto() {
+    const btnRemover = document.getElementById('btnRemover');
+    btnRemover.addEventListener('click', mensagemDeConfirmacao);
+}
+
+function mensagemDeConfirmacao() {
+    Templates.removerProduto();
+
+    const botaoFechar = document.getElementById('btnFechar');
+    botaoFechar.addEventListener('click', () => {
+        const container = document.querySelector('.container');
+        document.body.removeChild(container);
+    });
+}
+excluirProduto();
